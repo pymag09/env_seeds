408e662737d1f4aa9e0256885c136a56c7e862e4
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-10 18:07:04 +0200

    test shared library

--- vars/RunBodgeit.groovy
+++ vars/RunBodgeit.groovy
@@ -13,22 +13,19 @@ def call(body) {
     def dockeris=new Docker().IsDockerInstalled
     println "BUILDING...."
     node() {
-        stage("Build bodgeit") {
-            if (dockeris) {
-                echo "BUILDING..."
-                git url: "https://github.com/psiinon/bodgeit.git"
-                sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
-                withAnt(installation: 'ant-latest') {
-                    sh:
-                    ant build test
-                }
-            } else {
-                println "Docker is not installed"
+        if (dockeris) {
+            echo "BUILDING..."
+            git url: "https://github.com/psiinon/bodgeit.git"
+            sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
+            withAnt(installation: 'ant-latest') {
+                sh:
+                ant build test
             }
+        } else {
+            println "Docker is not installed"
         }
-        stage("Create container") {
-            if (dockeris)
-                sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
+        if (dockeris)
+            sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
         }
     }
 }
