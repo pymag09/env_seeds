98beb2fff9d10114a0fef8fc76f179716f191fdf
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-10 14:16:31 +0200

    test shared library

--- vars/RunBodgeit.groovy
+++ vars/RunBodgeit.groovy
@@ -2,18 +2,13 @@ package pymag.dsl
 
 def call() {
     if (new IsDockerInstalled().check) {
-        node {
-            stage("Build bodgeit site") {
-                git url: "https://github.com/psiinon/bodgeit.git"
-                sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
-                withAnt(installation: 'ant-latest') {
-                    sh:
-                    ant build test
-                }
-            }
-            stage("Create container") {
-                sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
-            }
+
+        git url: "https://github.com/psiinon/bodgeit.git"
+        sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
+        withAnt(installation: 'ant-latest') {
+            sh:
+            ant build test
         }
+        sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
     }
 }
