cdb1379dcc18f327900771efc844c3dd726ad482
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-10 18:01:46 +0200

    test shared library

--- vars/RunBodgeit.groovy
+++ vars/RunBodgeit.groovy
@@ -12,19 +12,23 @@ def call(body) {
 
     def dockeris=new Docker().IsDockerInstalled
     println "BUILDING...."
-    stage("Build bodgeit") {
-        if (dockeris) {
-            echo "BUILDING..."
-            git url: "https://github.com/psiinon/bodgeit.git"
-            sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
-            withAnt(installation: 'ant-latest') {
-                sh:
-                ant build test
+    node() {
+        stage("Build bodgeit") {
+            if (dockeris) {
+                echo "BUILDING..."
+                git url: "https://github.com/psiinon/bodgeit.git"
+                sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
+                withAnt(installation: 'ant-latest') {
+                    sh:
+                    ant build test
+                }
+            } else {
+                println "Docker is not installed"
             }
-        } else { println "Docker is not installed"}
-    }
-    stage("Create container") {
-        if (dockeris)
-            sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
+        }
+        stage("Create container") {
+            if (dockeris)
+                sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
+        }
     }
 }
