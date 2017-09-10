aa07f76fbf81dbb0cc4f3f47688253b7ab348287
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-10 17:59:44 +0200

    test shared library

--- vars/RunBodgeit.groovy
+++ vars/RunBodgeit.groovy
@@ -5,28 +5,26 @@ import pymag.dsl.Docker
 @NonCPS
 def call(body) {
 
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+//    def config = [:]
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
 
-//    def dockeris=new Docker().IsDockerInstalled
-//    println "BUILDING...."
-//    if (dockeris) {
-//        node {
-            stage("Build bodgeit") {
-                echo "BUILDING..."
-                git url: "https://github.com/psiinon/bodgeit.git"
-                sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
-                withAnt(installation: 'ant-latest') {
-                    sh:
-                    ant build test
-                }
+    def dockeris=new Docker().IsDockerInstalled
+    println "BUILDING...."
+    stage("Build bodgeit") {
+        if (dockeris) {
+            echo "BUILDING..."
+            git url: "https://github.com/psiinon/bodgeit.git"
+            sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
+            withAnt(installation: 'ant-latest') {
+                sh:
+                ant build test
             }
-            stage("Create container") {
-                sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
-            }
-        //}
-    //} else { println "Docker is not installed"}
-
+        } else { println "Docker is not installed"}
+    }
+    stage("Create container") {
+        if (dockeris)
+            sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
+    }
 }
