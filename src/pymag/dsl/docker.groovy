f38c1d9750f497d64de7f5ebf9b5b5202ef24043
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-10 13:52:41 +0200

    test shared library

---
+++ src/pymag/dsl/docker.groovy
@@ -0,0 +1,30 @@
+package pymag.dsl
+
+class IsDockerInstalled implements Serializable {
+	private boolean check
+
+	IsDockerInstalled(){
+		this.check=false
+		Check()
+	}
+
+	def getCheck(){
+		return this.check
+	}
+	private void Check(){
+		def sout = new StringBuilder()
+		def serr = new StringBuilder()
+		def proc = 'sudo docker info'.execute()
+
+		proc.consumeProcessOutput(sout, serr)
+		proc.waitForOrKill(1000)
+		if (!proc.exitValue()) {
+			def info=sout.toString().split("\\r?\\n").each{ it.trim() }.collectEntries {
+				def key_val=it.split(": ")
+				[(key_val.first()):key_val.last()]
+			}
+			println("Docker version: ${info["Server Version"]}")
+			this.check=true
+		}
+	}
+}
