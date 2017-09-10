f22aee7832aba831e36287c1d50fe01acb3facee
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-10 18:21:10 +0200

    test shared library

--- vars/RunBodgeit.groovy
+++ vars/RunBodgeit.groovy
@@ -4,21 +4,25 @@ import pymag.dsl.Docker
 
 @NonCPS
 def call(steps) {
-    def dockeris=new Docker().IsDockerInstalled
+    def dockeris = new Docker().IsDockerInstalled
     println "BUILDING...."
-    stage("Build and put into container") {
-        if (dockeris) {
-            steps.echo "BUILDING..."
-            steps.git url: "https://github.com/psiinon/bodgeit.git"
-            steps.sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
-            steps.withAnt(installation: 'ant-latest') {
-                sh:
-                ant build test
+    node(){
+        stage("Build and put into container") {
+            if (dockeris) {
+                steps.echo "BUILDING..."
+                steps.git url: "https://github.com/psiinon/bodgeit.git"
+                steps.sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
+                steps.withAnt(installation: 'ant-latest') {
+                    sh:
+                    ant build test
+                }
+            } else {
+                println "Docker is not installed"
             }
-        } else {
-            println "Docker is not installed"
         }
-        if (dockeris)
-            steps.sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
+        stage("Run container") {
+            if (dockeris)
+                steps.sh 'docker run -d -v /var/lib/jenkins/workspace/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
+        }
     }
 }
