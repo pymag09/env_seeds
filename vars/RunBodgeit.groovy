13772ea0857762f71cc0a2796b7198ca7197c89f
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-17 21:58:18 +0200

    g

--- vars/RunBodgeit.groovy
+++ vars/RunBodgeit.groovy
@@ -16,6 +16,7 @@ def call() {
         }
         stage("Run container") {
             def dockeris = new Docker("bodgeit").IsDockerInstalled
+            echo "${dockeris}"
             if (dockeris)
                 sh 'sudo docker run -d -v $WORKSPACE/bodgeit/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
             else
