431ffabdad9e94f0cba72822ae24da6a38b094cf
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-09-23 23:37:27 +0200

    ant variable

--- vars/BuildBodgeit.groovy
+++ vars/BuildBodgeit.groovy
@@ -8,10 +8,10 @@ def call(body) {
 
     node {
         ansiColor('xterm') {
-            stage("Build and put into container") {
-                echo "\\e[31;1m++++++++ GIT ++++++++\e[0m"
+            stage("\\e[31;1mBuild and put into container\\e[0m") {
+                echo "\\e[31;1m++++++++ GIT ++++++++\\e[0m"
                 git branch: 'master', url: 'https://github.com/psiinon/bodgeit.git'
-                echo "\\e[31;1m++++++++ MKDIR ++++++++\e[0m"
+                echo "\\e[31;1m++++++++ MKDIR ++++++++\\e[0m"
                 sh 'mkdir -p $WORKSPACE/build/WEB-INF/classes'
                 echo "\\e[31;1m++++++++ ANT ++++++++\\e[0m"
                 withAnt(installation: "${config.anttool}") {
