6f844a371b7602e97ff107429e78b9b047f06820
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-11-04 22:35:27 +0100

    refactoring

--- jenkins_zap/provisioning/groovy/create_all_jobs.groovy
+++ jenkins_zap/provisioning/groovy/create_all_jobs.groovy
@@ -1,24 +1,32 @@
-// Global variables
-def top_folder_name="bodgeit-pipeline"
-def pipeline_name="bodgeit-delivery-pipeline"
-def build_step_name="bodgeit-build"
-def deploy_step_name="bodgeit-deploy"
-def test_step_name="bodgeit-zap"
+class JobRoot {
+    final protected String top_folder_name="bodgeit-pipeline"
+    final protected String pipeline_name="bodgeit-delivery-pipeline"
+    final protected String build_step_name="bodgeit-build"
+    final protected String deploy_step_name="bodgeit-deploy"
+    final protected String test_step_name="bodgeit-zap"
+    protected def dslFactory
 
-// Top level folder
-folder("${top_folder_name}") {
-    displayName("${top_folder_name}")
-    description('Delivery pipeline for bodgeit site')
+    public void build() {
+        DSLcode()
+    }
 }
-
-// Entry point. Delivery pipeline
-pipelineJob("${top_folder_name}/${pipeline_name}") {
-    displayName("${pipeline_name}")
-    definition {
-        cps {
-            script('''
-node{
-    stage("Build bodgeut from sources code using Ant"){
+class PipelineDSLTemplate extends JobRoot {
+    final protected Closure DSLcode = {
+        dslFactory.pipelineJob("${top_folder_name}/${job_name}") {
+            displayName("${job_name}")
+            definition {
+                cps {
+                    script(inlineScript)
+                    sandbox(sandbox=false)
+                }
+            }
+        }
+    }
+}
+class MainPipelineJob extends PipelineDSLTemplate {
+    final String job_name = pipeline_name
+    final private String inlineScript = '''node{
+    stage("Build bodgeit from source code using Ant"){
         build \'''' + "${top_folder_name}/${build_step_name}" + '''\'
     }
     stage("Deploy bodgeit in docker containter"){
@@ -29,169 +37,177 @@ node{
     }
 }
 '''
-)
-            sandbox(sandbox=false)
-
-        }
-    }
+  MainPipelineJob(def dslFactory){ this.dslFactory = dslFactory }
 }
-
-// Build job
-pipelineJob("${top_folder_name}/${build_step_name}") {
-    displayName("${build_step_name}")
-    definition {
-        cps {
-            script('''
+class BuildJob extends PipelineDSLTemplate {
+    final String job_name = build_step_name
+    final private String inlineScript = '''
 @Library('bodgeit') _
 
 BuildBodgeit{
     anttool = "ant-latest"
 }
 '''
-)
-            sandbox(sandbox=false)
-
-        }
-    }
+  BuildJob(def dslFactory){ this.dslFactory = dslFactory }
 }
-// Deploy job
-pipelineJob("${top_folder_name}/${deploy_step_name}") {
-    displayName("${deploy_step_name}")
-    definition {
-        cps {
-            script('''
-@Library("bodgeit") _
+class DeployJob extends PipelineDSLTemplate {
+    final String job_name = deploy_step_name
+    final private String inlineScript = '''@Library("bodgeit") _
 
 node('master'){
-    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '2')), [$class: 'CopyArtifactPermissionProperty', projectNames: '*'], parameters([string(defaultValue: '', description: 'Job name to take artefact from', name: 'upstream_job')]), pipelineTriggers([])])
-    step([$class: 'CopyArtifact', filter: 'build/bodgeit.war', fingerprintArtifacts: true, flatten: true, projectName: "${params.upstream_job}"])
+    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '',
+                                artifactNumToKeepStr: '',
+                                daysToKeepStr: '',
+                                numToKeepStr: '2')),
+                [$class: 'CopyArtifactPermissionProperty', projectNames: '*'],
+                parameters([string(defaultValue: '', description: 'Job name to take artefact from', name: 'upstream_job')]), pipelineTriggers([])])
+    step([$class: 'CopyArtifact',
+        filter: 'build/bodgeit.war',
+        fingerprintArtifacts: true,
+        flatten: true,
+        projectName: "${params.upstream_job}"])
 }
-
 RDocker{
     command = 'docker run -d -v $WORKSPACE/build/bodgeit.war:/usr/local/tomcat/webapps/bodgeit.war --name bodgeit -p 8181:8080 tomcat'
 }
-
 '''
-)
-            sandbox(sandbox=false)
-
-        }
-    }
+  DeployJob(def dslFactory) { this.dslFactory = dslFactory }
 }
-// Security test job
-freeStyleJob("${top_folder_name}/${test_step_name}") {
-    logRotator(-1,2,-1,-1)
-    authenticationToken('VANDHRV73hbc5dsj')
-    steps {
-        zapBuilder {
-            startZAPFirst(false)
-            zapHost("localhost")
-            zapPort("9090")
-            zaproxy {
-                autoInstall(true)
-                toolUsed("zap-2.6.0")
-                zapHome("/var/lib/jenkins/ZAP")
-                alertFilters("")
-                loggedOutIndicator("")
-                jdk("InheritFromJob")
-                timeout(60)
-                zapSettingsDir("/var/lib/jenkins/ZAP")
-                loadSession("")
-                autoLoadSession(false)
-                sessionFilename("bodgeit")
-                removeExternalSites(false)
-                internalSites("")
-                contextName("zap \${BUILD_ID}")
-                includedURL("http://localhost:8181/bodgeit.*")
-                excludedURL("")
-                authMode(true)
-                username("zap@test.com")
-                password("test123")
-                loggedInIndicator(".*\\Qlogout=\\E.*")
-                authMethod("FORM_BASED")
-                loginURL("http://localhost:8181/bodgeit/login.jsp")
-                usernameParameter("username")
-                passwordParameter("password")
-                extraPostData("")
-                authScript("")
-                authScriptParams {}
-                targetURL("http://localhost:8181/bodgeit/")
-                spiderScanURL(true)
-                spiderScanRecurse(true)
-                spiderScanSubtreeOnly(false)
-                spiderScanMaxChildrenToCrawl(0)
-                ajaxSpiderURL(false)
-                ajaxSpiderInScopeOnly(false)
-                activeScanURL(true)
-                activeScanRecurse(true)
-                activeScanPolicy("Default Policy")
-                generateReports(true)
-                selectedReportMethod("DEFAULT_REPORT")
-                deleteReports(true)
-                reportFilename("JENKINS_ZAP_VULNERABILITY_REPORT_\${BUILD_ID}")
-                selectedReportFormats(["xml", "html"])
-                selectedExportFormats(["xml", "json"])
-                exportreportTitle("")
-                exportreportBy("")
-                exportreportFor("")
-                exportreportScanDate("")
-                exportreportReportDate("")
-                exportreportScanVersion("")
-                exportreportReportVersion("")
-                exportreportReportDescription("")
-                exportreportAlertHigh(false)
-                exportreportAlertMedium(false)
-                exportreportAlertLow(false)
-                exportreportAlertInformational(false)
-                exportreportCWEID(false)
-                exportreportWASCID(false)
-                exportreportDescription(false)
-                exportreportOtherInfo(false)
-                exportreportSolution(false)
-                exportreportReference(false)
-                exportreportRequestHeader(false)
-                exportreportResponseHeader(false)
-                exportreportRequestBody(false)
-                exportreportResponseBody(false)
-                jiraCreate(false)
-                jiraProjectKey("")
-                jiraAssignee("")
-                jiraAlertHigh(false)
-                jiraAlertMedium(false)
-                jiraAlertLow(false)
-                jiraFilterIssuesByResourceType(false)
-                cmdLinesZAP {
-                    zapCmdLine {
-                        cmdLineOption("-host")
-                        cmdLineValue("localhost")
-                    }
-                    zapCmdLine {
-                        cmdLineOption("-port")
-                        cmdLineValue("9090")
-                    }
-                    zapCmdLine {
-                        cmdLineOption("-dir")
-                        cmdLineValue("/var/lib/jenkins/ZAP")
-                    }
-                    zapCmdLine {
-                        cmdLineOption("-config")
-                        cmdLineValue("database.compact=true")
-                    }
+class RunZAPJob extends JobRoot {
+    final String job_name = test_step_name
+    final private Closure DSLcode = {
+        dslFactory.freeStyleJob("${top_folder_name}/${test_step_name}") {
+            logRotator(-1, 2, -1, -1)
+            authenticationToken('VANDHRV73hbc5dsj')
+            steps {
+                zapBuilder {
+                    startZAPFirst(false)
+                    zapHost("localhost")
+                    zapPort("9090")
+                    zaproxy {
+                        autoInstall(true)
+                        toolUsed("zap-2.6.0")
+                        zapHome("/var/lib/jenkins/ZAP")
+                        alertFilters("")
+                        loggedOutIndicator("")
+                        jdk("InheritFromJob")
+                        timeout(60)
+                        zapSettingsDir("/var/lib/jenkins/ZAP")
+                        loadSession("")
+                        autoLoadSession(false)
+                        sessionFilename("bodgeit")
+                        removeExternalSites(false)
+                        internalSites("")
+                        contextName("zap \${BUILD_ID}")
+                        includedURL("http://localhost:8181/bodgeit.*")
+                        excludedURL("")
+                        authMode(true)
+                        username("zap@test.com")
+                        password("test123")
+                        loggedInIndicator(".*\\Qlogout=\\E.*")
+                        authMethod("FORM_BASED")
+                        loginURL("http://localhost:8181/bodgeit/login.jsp")
+                        usernameParameter("username")
+                        passwordParameter("password")
+                        extraPostData("")
+                        authScript("")
+                        authScriptParams {}
+                        targetURL("http://localhost:8181/bodgeit/")
+                        spiderScanURL(true)
+                        spiderScanRecurse(true)
+                        spiderScanSubtreeOnly(false)
+                        spiderScanMaxChildrenToCrawl(0)
+                        ajaxSpiderURL(false)
+                        ajaxSpiderInScopeOnly(false)
+                        activeScanURL(true)
+                        activeScanRecurse(true)
+                        activeScanPolicy("Default Policy")
+                        generateReports(true)
+                        selectedReportMethod("DEFAULT_REPORT")
+                        deleteReports(true)
+                        reportFilename("JENKINS_ZAP_VULNERABILITY_REPORT_\${BUILD_ID}")
+                        selectedReportFormats(["xml", "html"])
+                        selectedExportFormats(["xml", "json"])
+                        exportreportTitle("")
+                        exportreportBy("")
+                        exportreportFor("")
+                        exportreportScanDate("")
+                        exportreportReportDate("")
+                        exportreportScanVersion("")
+                        exportreportReportVersion("")
+                        exportreportReportDescription("")
+                        exportreportAlertHigh(false)
+                        exportreportAlertMedium(false)
+                        exportreportAlertLow(false)
+                        exportreportAlertInformational(false)
+                        exportreportCWEID(false)
+                        exportreportWASCID(false)
+                        exportreportDescription(false)
+                        exportreportOtherInfo(false)
+                        exportreportSolution(false)
+                        exportreportReference(false)
+                        exportreportRequestHeader(false)
+                        exportreportResponseHeader(false)
+                        exportreportRequestBody(false)
+                        exportreportResponseBody(false)
+                        jiraCreate(false)
+                        jiraProjectKey("")
+                        jiraAssignee("")
+                        jiraAlertHigh(false)
+                        jiraAlertMedium(false)
+                        jiraAlertLow(false)
+                        jiraFilterIssuesByResourceType(false)
+                        cmdLinesZAP {
+                            zapCmdLine {
+                                cmdLineOption("-host")
+                                cmdLineValue("localhost")
+                            }
+                            zapCmdLine {
+                                cmdLineOption("-port")
+                                cmdLineValue("9090")
+                            }
+                            zapCmdLine {
+                                cmdLineOption("-dir")
+                                cmdLineValue("/var/lib/jenkins/ZAP")
+                            }
+                            zapCmdLine {
+                                cmdLineOption("-config")
+                                cmdLineValue("database.compact=true")
+                            }
 
+                        }
+                    }
+                }
+            }
+            publishers {
+                archiveArtifacts('reports/*')
+                publishHtml {
+                    report('reports') {
+                        reportName('HTML Report')
+                        reportFiles('JENKINS_ZAP_VULNERABILITY_REPORT_${BUILD_ID}.html')
+                        keepAll()
+                        allowMissing()
+                        alwaysLinkToLastBuild()
+                    }
                 }
             }
         }
     }
-    publishers {
-        archiveArtifacts('reports/*')
-        publishHtml {
-            report('reports') {
-                reportName('HTML Report')
-                reportFiles('JENKINS_ZAP_VULNERABILITY_REPORT_${BUILD_ID}.html')
-                keepAll()
-                allowMissing()
-                alwaysLinkToLastBuild()
-            }
+  RunZAPJob(def dslFactory) { this.dslFactory = dslFactory }
+}
+class TopFolder extends JobRoot {
+    final private Closure DSLcode = {
+        dslFactory.folder("${top_folder_name}") {
+            displayName("${top_folder_name}")
+            description('Delivery pipeline for bodgeit site')
         }
     }
+    TopFolder(def dslFactory) { this.dslFactory = dslFactory }
 }
+
+def jobs = [new TopFolder(this),
+            new MainPipelineJob(this),
+            new BuildJob(this),
+            new DeployJob(this),
+            new RunZAPJob(this)]
+jobs.each { it.build() }
